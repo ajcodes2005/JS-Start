d3ee7871eacfd670a2495fe0120aaa372d998ac9
ajcodes2005 <ajayyadav182764@gmail.com>
2025-09-04 12:49:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 357246 
+let accountEmail = "primenumber2@gmail.com"
+var accountPassword = "Ajay123@"
+accountCity = "Ranchi"
+let accountState;
+
+
+// accountId = 2
+console.log(accountId);
+console.log(accountEmail);
+
+
+accountEmail = "irrationalnumber2@gmail.com"
+accountPassword = "Ajay123$"
+accountCity = "Jamshedpur"
+
+console.table( [accountId, accountEmail, accountPassword, accountCity, accountState] )
+
+
