888517e9aff7953581aebb6d7d67a1da5b20f1f5
ajcodes2005 <ajayyadav182764@gmail.com>
2025-09-07 04:29:52 +0000

    Operation datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,94 @@
+// let num = 456;
+// console.log(num);
+// console.log(typeof 456);
+
+
+// const value = 10.8;
+// console.log(value);
+// console.log(typeof 10.8);
+
+// datatype conversion now (implicit) :
+
+/* Addition of string and number below */
+
+ /* console.log("99" + 9);
+console.log(typeof ("99"+9));
+
+console.log (17+7);
+console.log(typeof (17+7));
+
+console.log ("11" + "11");
+console.log(typeof ("11" + "11")); */
+
+ /* console.log ("18"+"17");
+console.log ("18" - "17");
+console.log (typeof ("18" - "17"));
+console.log (typeof ("18" + "17" )); */
+
+// NOTE : addition of string isn't like adding two integers. It is like ("77" + "9") = 779 
+// NOTE : subtraction of string is like integer. i.e. ( "29"- "9") = 20 
+// NOTE : in addition of string the type of we get is string but in subtraction of two string we get typeof is number
+
+// subtraction of "string" and number below :
+
+/* console.log (17-7);
+console.log (typeof (17-7));
+
+console.log (65 + (-11));
+console.log (typeof (65 + (-11)));
+
+console.log (-99 + 19);
+console.log (typeof (-99 + 19));
+
+console.log ("25" - "10");
+console.log (typeof ("25" - "10"));
+
+console.log ( "31" - "20");
+console.log (typeof ("31" - "20"));
+
+console.log ( "41" - 33);
+console.log ( typeof ("41" - 33)); */
+
+// NOTE: when we subtract number from number we get number like operation in integers. 
+// when we subtract string from string operation works same like integers.
+// number - number = number => typeof is also number
+// string - string = number ( operation like integers)
+// string - number + number ( operation like integers)
+
+
+// We will do multiplication now with number and string
+
+
+console.log (5*5);
+console.log (typeof (5*5));
+
+console.log ( 7*(-7) );
+console.log (typeof (7*(-7)));
+
+console.log ( "15" * 7);
+console.log (typeof ("15" * 7));
+
+console.log ("13" * "17");
+console.log (typeof ("13" * "17"));
+
+console.log ("-21" * "-17");
+console.log (typeof ("-21" * "-17"));
+
+// NOTE: Multiplication of number and number , number and string , string and string work same like integers.
+// and typeof of multiplication of number and  number , number and string and string and string give number type.
+
+// Now we wil do divison in number and string 
+console.log ( 65/19 );
+console.log (typeof 65/19);
+console.log (75/7.5);
+console.log (typeof 75/7.5);
+
+
+
+
+
+
+
+
+
+
