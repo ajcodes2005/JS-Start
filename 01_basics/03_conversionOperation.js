2589f9006f79e07faa0fe7fd61fb8f1476fd06e7
ajcodes2005 <ajayyadav182764@gmail.com>
2025-09-10 04:11:35 +0000

    Datatype Conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -176,6 +176,7 @@ console.log(numberToBoolean);
 
 // Above We've done conversion of value like String, Boolean to Number()
 
+/*
 let str = "3.14159";
 let int = parseInt(str);
 console.log(int);
@@ -193,6 +194,183 @@ let one = (parseInt(userIsBot));
 console.log(one);
 console.log(typeof one);
 
+let userName = ("shekhar");
+let num = (parseInt ("userName"));
+console.log(num);
+*/
+
+//NOTE: Above we've done conversion of string to parseInt.
+//NOTE: This conversion means we get value in integer of string.
+
+/*
+let str = "3.14159";
+let float = parseFloat(str);
+console.log(float);
+console.log(typeof float);
+
+let str2 = "0.000001010";
+let float2 = parseFloat(str2);
+console.log(float2);
+console.log(typeof float2);
+
+let string = "6.78qrs";
+let decimalNumber = parseFloat(string);
+console.log(decimalNumber);
+
+
+let invalid = "rehan";
+let notNumber = parseFloat(invalid);
+console.log(notNumber);
+
+let invalid2 = "ajey";
+console.log(parseFloat(invalid2));
+*/
+
+/*
+let userAge = "19.8";
+console.log(parseFloat(userAge));
+console.log(parseInt(19.8));
+
+let userPassword = "1827125";
+console.log(parseFloat(userPassword));
+console.log(parseInt(userPassword));
+
+let userId = "134.134";
+console.log(parseFloat(userId));
+console.log(parseInt(userId));
+*/
+
+//NOTE: Above we've done the conversion of string to parseInt( ) and parseFloat( ).
+// and it is like parseInt( ) gives the integer value and parseFloat( ) gives decimal value.
+
+/*
+let str = "33";
+let num = +str;
+console.log(num);
+
+let birthYear = "2006";
+let number = +birthYear;
+console.log(number);
+
+let address = "Jamshedpur";
+let num2 = +address;
+console.log(num2);
+
+let invalid = +"hello";
+console.log(invalid);
+
+let bool = +true;
+console.log(bool);
+*/
+//NOTE: Above we've done conversion of string and boolean to number by +unary plus operator method.
+// in this method we assign the value of string, boolean and during putting value we add +
+// before the name of variable we add the sign +.
+
+/*
+let digit = 689;
+let str = String(digit);
+console.log(str);
+console.log(typeof str);
+
+let year = String(2025);
+console.log(year);
+console.log(typeof year);
+
+let correct = String(true);
+console.log(correct);
+console.log(typeof correct);
+*/
+//NOTE: Above is conversion of number,boolean to string.
+
+/*
+let num = 359;
+let str = num.toString();
+console.log(str);
+console.log(typeof str);
+
+let num2 = 2525;
+let str2 = num2.toString();
+console.log(str2);
+console.log(typeof str2);
+
+let userIsAdult = true;
+let string = userIsAdult.toString();
+console.log(string);
+console.log(typeof string);
+*/
+
+/*
+let str = "ajay";
+let valid = Boolean(str);
+console.log(valid);
+console.log(typeof valid);
+
+let value = "";
+console.log(Boolean(value));
+console.log (typeof value);
+
+let bool = 0;
+console.log(Boolean(bool));
+console.log(typeof (bool));
+
+let bool2 = "hey";
+console.log(!!bool2);
+console.log(typeof(!!bool2));
+*/
+//NOTE: Above we've done Doublr NOT Operator (!!)
+// it is a shorthand way to convert a value to a boolean
+
+/*
+let num = 675;
+let numObj = Object(num);
+console.log(numObj);
+console.log(typeof numObj);
+
+let year = 2005;
+let yearObj = Object(year);
+console.log(yearObj);
+console.log(typeof yearObj);
+
+let minutes = Object(60);
+console.log(minutes);
+console.log(typeof minutes);
+
+let sem = Object(1);
+console.log(sem);
+console.log(typeof sem);
+
+let userName = "Ajay";
+let user = Object(userName);
+console.log(user);
+console.log(typeof user)
+
+let uid = "sam165";
+let uidObject = Object(uid);
+console.log(uidObject);
+*/
+//Note: Above we've done the conversion of number to Number object and string to String object.
+/*
+let str = "sweety";
+let arr = [...str];
+console.log(arr);
+console.log(typeof arr)
+
+let str2 = "welcome";
+let arr2 = [...str2];
+console.log(arr2);
+console.log(typeof arr2);
+*/
+//note: we've done conversion of string to array.
+
+//from here we'll do some practise of real world examples.
+let userInput = "50.75";
+let price = parseFloat(userInput);
+let tax = price * 0.1 ;
+console.log(tax);
+
+let score = 95;
+let message = "Your score is:" + String(score);
+console.log(message);
 
 
 
