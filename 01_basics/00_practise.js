75c87b9f77219808ed9ca79c12da4534df287c12
ajcodes2005 <ajayyadav182764@gmail.com>
2025-09-12 04:06:34 +0000

    practise

---
+++ 01_basics/00_practise.js
@@ -0,0 +1,37 @@
+let add = "5"+3;
+//console.log(add);
+//console.log(typeof add)
+
+let alphabet = "xyzabc";
+//console.log(Number(alphabet));
+//console.log(typeof alphabet);
+
+let day = "120925";
+//console.log(Number(day));
+//console.log(typeof day);
+
+let str = "123.321";
+//console.log(parseInt("123.321"));
+//console.log(typeof str);
+
+let str2 = "123xyz";
+//console.log(parseInt(str2));
+//console.log(typeof str2);
+
+let decimal = "1.414";
+//console.log(parseFloat(decimal));
+//console.log(typeof decimal);
+
+let pi = "irrational";
+//console.log(+(pi));
+
+let xxx = "478efq";
+//console.log(+(xxx));
+//console.log(typeof (xxx));
+
+let xvx = "2010";
+//console.log(+(xvx));
+
+let largest4digit = "9999";
+console.log(Number("9999"));
+console.log(isNan Number(("9999")));
