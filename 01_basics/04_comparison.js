b41375724e041ab7d77d328a7dd1905701358615
ajcodes2005 <ajayyadav182764@gmail.com>
2025-09-14 04:06:46 +0000

    Comparing Datatypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+//console.log(2 > 2);
+//console.log(5 < 7);
+//console.log(6 <= 6);
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(2 == 1);
+//console.log(4 != 3);
+//console.log(7 != 7);
+
+//console.log(null == 0);
+//console.log(null >= 0);
+//console.log(null > 0);
+
+//console.log(undefined == 0);
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+//console.log(undefined >= 0);
+
+//NOTE : == , === these are different and they have different meaning than > , < , >= etc..
+
+console.log("5" == 5);
+console.log("4" === 4);
+//NOTE: == it convert string to number but === it doesn't convert.
